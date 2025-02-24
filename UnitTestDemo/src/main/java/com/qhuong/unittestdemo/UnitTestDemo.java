8c8d001ede71f0b840d6b5f8a2fd7241869674f9
quynhhuongtran314@gmail.com <quynhhuongtran314@gmail.com>
2025-02-24 13:31:23 +0700

    init project

---
+++ UnitTestDemo/src/main/java/com/qhuong/unittestdemo/UnitTestDemo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.qhuong.unittestdemo;
+
+/**
+ *
+ * @author admin
+ */
+public class UnitTestDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
